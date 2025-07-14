75944d40162ad66d1967fb27de6255875d829662
awhitmana0 <andrew.whitman@okta.com>
2025-07-13 18:50:59 -0700

    added custom css file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./custom-acul-styles.css"; // <-- NEW IMPORT HERE
 import { loadAndSetMockContext } from "@/utils/screen/mockContextLoader";
 
 async function initializeApp() {
